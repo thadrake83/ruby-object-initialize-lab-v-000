470de294a59e472884d1b161e014141cdceb68b2
Drake Rentrop <drakerentrop@gmail.com>
2018-09-14 20:09:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,11 @@
-1.	class Person
-2.	def initialize(name)
-3.	@name = name
-4.	end
-5.	 
-6.	def name=(name)
-7.	@name = name
-8.	end
-9.	 
-10.	def name
-11.	@name
-12.	end
-13.	end
+class Person
+  def initialize(breed)
+    @breed = breed
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed 
+  end
+end
